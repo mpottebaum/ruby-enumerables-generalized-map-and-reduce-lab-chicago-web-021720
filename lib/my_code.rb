90920ea8c4cef1228ebfb0019f9c928d9c530db2
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-27 01:42:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,8 @@ def reduce(array, starting_point = 0)
   total = starting_point
   index = 0
   while index < array.length do
-    
+    yield(array[index], total)
+    index += 1
   end
   total
 end
