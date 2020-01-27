a5fc4f29c38880427cc499282a0a66aaab8a9855
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-27 01:41:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
 # Your Code Here
+def map(array)
+  index = 0
+  while index < array.length do
+    yield(array[index])
+    index += 1
+  end
+  array
+end
+
+def reduce(array, starting_point = 0)
+  total = starting_point
+  index = 0
+  while index < array.length do
+    
+  end
+  total
+end
